cd9e4842833cc8637ae975f66aa72f08f04ba313
Clayton Hill <clayman0123@yahoo.com>
2020-05-16 16:38:43 -0600

    app functioning properly

--- logic.js
+++ logic.js
@@ -1,3 +1,32 @@
 var toss = Math.floor((Math.random() * 2) + 1);
 
+var heads = 0;
 
+var tails = 0;
+
+$("document").ready(function onStart() {
+  $("#face").hide();
+  $("#back").hide();
+})
+
+$("button").click(function coinToss() {
+  if (toss == 1) {
+    $("#double").hide();
+    $("#face").show();
+    $("#back").hide();
+    heads++;
+    $("#heads").text("Heads: " + heads);
+    reset();
+  } else {
+    $("#double").hide();
+    $("#face").hide();
+    $("#back").show();
+    tails++;
+    $("#tails").text("Tails: " + tails);
+    reset();
+  }
+})
+
+function reset() {
+  toss = Math.floor((Math.random() * 2) + 1);
+}
